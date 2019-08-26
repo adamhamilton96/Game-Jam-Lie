e75a56f66536463b549c78a14fd1a952cbb65fba
Benn Henderson <bhenderson05@qub.ac.uk>
2019-08-26 15:28:30 +0100

    Added the ability to jump with up arrow key. Character is now a scene vs a series of nodes.

--- Character.gd
+++ Character.gd
@@ -1,9 +1,15 @@
 extends KinematicBody2D
 
-const gravity = 200.0
+const gravity = 20
 const walkSpeed = 200
 
+#Jump 
+export var fallMultiplier = 2 
+export var lowJumpMultiplier = 10 
+export var jumpVelocity = 400 #Jump height
+
 var velocity = Vector2()
+
 
 func _physics_process(delta):
 	velocity.y += delta * gravity
@@ -15,3 +21,22 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 	move_and_slide(velocity, Vector2(0, -1))
+	
+	handleJump()
+
+
+func handleJump():
+	# Jump code from this post -> https://godotengine.org/qa/25897/2d-platformer-how-to-control-jump-height-mario-like-jumps
+	velocity.y += gravity 
+	
+	if velocity.y > 0: 
+	    velocity += Vector2.UP * (-9.81) * (fallMultiplier) 
+	
+	elif velocity.y < 0 && Input.is_action_just_released("ui_up"): 
+	    velocity += Vector2.UP * (-9.81) * (lowJumpMultiplier)
+	
+	if is_on_floor():
+	    if Input.is_action_just_pressed("ui_up"): 
+	        velocity = Vector2.UP * jumpVelocity
+	
+	velocity = move_and_slide(velocity, Vector2(0,-1))
